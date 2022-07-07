ef29b5eff7096a93ddb1d418867c5aa31c3b6876
khandaker.maruf <khandaker.maruf@therapservices.net>
2022-07-07 09:19:46 +0600

    init

---
+++ src/main/java/net/therap/jokesrandom/JokesRandomApplication.java
@@ -0,0 +1,13 @@
+package net.therap.jokesrandom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JokesRandomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JokesRandomApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/therap/jokesrandom/JokesRandomApplicationTests.java
@@ -0,0 +1,13 @@
+package net.therap.jokesrandom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JokesRandomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
